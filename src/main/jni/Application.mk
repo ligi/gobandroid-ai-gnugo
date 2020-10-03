639520faa146f9b6c6ea57d2b226c0b8af7a2754
Sudeep Duggal <5505558+duggalsu@users.noreply.github.com>
2020-10-03 22:04:20 +0530

    Updated app
    - Removed unsupported ABIs - armeabi mips mips64
    - Updated gradle version
    - Updated buildToolsVersion
    - Updated compileSdkVersion
    - Updated targetSdkVersion

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,6 +4,6 @@ APP_PROJECT_PATH := $(call my-dir)/..
 APP_MODULES      := gnuGo-3.8
 APP_OPTIM	 := release
 APP_BUILD_SCRIPT := $(call my-dir)/project/Android.mk
-APP_ABI :=  armeabi armeabi-v7a x86 mips x86_64 mips64 arm64-v8a
+APP_ABI :=  armeabi-v7a x86 x86_64 arm64-v8a
 APP_PLATFORM := android-8
 
